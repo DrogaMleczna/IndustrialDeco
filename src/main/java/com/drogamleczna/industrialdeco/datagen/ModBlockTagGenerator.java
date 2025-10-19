5ac3496ebbaaa8c2e6fd66dae3bfc9233e331f58
marce <marcelir2d2@gmail.com>
2025-10-19 18:53:44 +0200

    Added block pickup via create's wrench

--- src/main/java/com/drogamleczna/industrialdeco/datagen/ModBlockTagGenerator.java
+++ src/main/java/com/drogamleczna/industrialdeco/datagen/ModBlockTagGenerator.java
@@ -7,7 +7,9 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
@@ -83,9 +85,36 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
         this.tag(BlockTags.MINEABLE_WITH_AXE)
                 .add(ModBlocks.PALLET.get())
                 .add(ModBlocks.BENCH.get());
+        this.tag(WRENCH_PICKUP)
+                .add(ModBlocks.STREET_LAMP.get())
+                .add(ModBlocks.CURVED_POLE.get())
+                .add(ModBlocks.DOUBLE_CURVED_POLE.get())
+                .add(ModBlocks.CORNER_POLE.get())
+                .add(ModBlocks.WALL_CURVED_POLE.get())
+                .add(ModBlocks.WIRE_POLE.get())
+                .add(ModBlocks.POLE_BASE.get())
+                .add(ModBlocks.LARGE_BASE.get())
+                .add(ModBlocks.MEDIUM_BASE.get())
+                .add(ModBlocks.POLE_BLOCK.get())
+                .add(ModBlocks.HAZARD_POLE.get())
+                .add(ModBlocks.WARNING_POLE.get())
+                .add(ModBlocks.CAMERA_POLE.get())
+                .add(ModBlocks.CAMERA_POLE_CORNER.get())
+                .add(ModBlocks.CAMERA_POLE_TRIPLE.get())
+                .add(ModBlocks.CAMERA_POLE_QUADRUPLE.get())
+                .add(ModBlocks.SECURITY_CAMERA.get())
+                .add(ModBlocks.SPLIT_POLE.get())
+                .add(ModBlocks.SWITCHBOARD.get())
+                .add(ModBlocks.WALL_SWITCHBOARD.get())
+                .add(ModBlocks.QUADRUPLE_CURVED_POLE.get())
+                .add(ModBlocks.CHIMNEY_BLOCK.get())
+                .add(ModBlocks.WALL_CHIMNEY.get())
+                .add(ModBlocks.BENT_CHIMNEY.get())
+                .add(ModBlocks.CEILING_OFFICE_LAMP.get())
+                .add(ModBlocks.WIRE_BOX.get())
+                .add(ModBlocks.CROSSBUCK_BLOCK.get())
+                .add(ModBlocks.METAL_FENCE_BLOCK.get());
     }
 
-    //private static TagKey<Block> create(String pName) {
-    //    return TagKey.create(Registries., new ResourceLocation(pName));
-    //}
+    TagKey<Block> WRENCH_PICKUP = BlockTags.create(new ResourceLocation("create", "wrench_pickup"));
 }
